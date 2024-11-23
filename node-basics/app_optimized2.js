8e93603d6a7d4cf859b4f1d9b8882b9cb72da040
Shashank Agasimani <shashankagasimani2@gmail.com>
2024-11-23 21:02:22 +0530

    New Optimized App.js

---
+++ node-basics/app_optimized2.js
@@ -0,0 +1,42 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const inputFilePath = process.argv[2] || path.join(__dirname, 'input.txt')
+const outputFilePath = process.argv[3] || path.join(__dirname, 'output.txt')
+
+const checkFileExists = async(filePath) => {
+    try{
+        await fs.access(filePath);
+        return true;
+    }
+    catch{
+        return false;
+    }
+};  
+
+const processFile = async() =>{
+    try{
+        const inputExists = await checkFileExists(inputFilePath);
+        if(!inputExists){
+            console.error(`Input File doesnt exist ${inputFilePath}`);
+            return;
+        }
+
+        const data=await fs.readFile(inputFilePath, 'utf8');
+        console.log("File Read Successful", data);
+
+
+        transformedData = data.toLowerCase();
+
+
+        await fs.writeFile(outputFilePath, transformedData);
+        console.log("File Write Successful")
+
+    }
+
+    catch(error){
+        console.error(`An Error occured ${error.message}`);
+    }
+};
+
+processFile()
